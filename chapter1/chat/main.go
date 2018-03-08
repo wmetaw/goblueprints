913f08d2e71fc51da2844e95de35bdea91a2b636
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-08 10:25:54 +0900

    chapter1-1

---
+++ chapter1/chat/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+// templは１つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// 一度だけ実行
+	t.once.Do(func() {
+		// テンプレートをコンパイル
+		t.templ = template.Must(template.ParseFiles(filepath.Join("chapter1/chat/templates", t.filename)))
+	})
+
+	// コンパイルしたテンプレートをResponseWriterに出力
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// webサーバーの開始
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
