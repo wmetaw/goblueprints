99c8b5de340874832e0ead732aacf66cd4d0a0f6
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-12 09:57:10 +0900

    1.2.3

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -13,3 +13,29 @@ type room struct {
 	// clientsには在室している全てのクライアントが保持される
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		// 参加
+		case client := <-r.join:
+			r.clients[client] = true
+
+			// 退室
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+
+			// 全てのクライアントにメッセージを転送
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
