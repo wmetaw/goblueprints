afb9524db2e27b39fc328e0754ca1c3c909687ab
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-16 13:14:44 +0900

    1.4.1.3

---
+++ chapter1/trace/tracer_test.go
@@ -0,0 +1,21 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは trace package")
+		if buf.String() != "こんにちは trace package\n" {
+			t.Errorf("'%s'という誤った文字列が検出されました", buf.String())
+		}
+	}
+}
