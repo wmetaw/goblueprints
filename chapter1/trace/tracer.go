8d0ec16c597a25931713f2f48a43d6eb5093559b
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-17 16:53:39 +0900

    1.4.1.5

--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -1,11 +1,24 @@
 package trace
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Tracer interface {
 	Trace(...interface{})
 }
 
+// io.Writerで受け付けるということは、ユーザーが出力先を自由に選べるということを意味する
 func New(w io.Writer) Tracer {
-	return nil
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
