2dbfe9324496cf63558f1632aa9f6f23c4142190
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-23 10:07:11 +0900

    3.3.3

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -67,6 +67,7 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.Handle("/room", r)
+	http.HandleFunc("/uploader", uploaderHandler)
 	http.HandleFunc("/auth/", loginHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
