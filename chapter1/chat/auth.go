cd535e327f7fc7bfbd6c055ad421853ea56dd9bd
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-03-27 10:12:52 +0900

    2.6.1

--- chapter1/chat/auth.go
+++ chapter1/chat/auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 	"log"
 	"net/http"
 	"strings"
@@ -46,6 +47,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	action := segs[2]
 	provider := segs[3]
 	switch action {
+
 	case "login":
 		provider, err := gomniauth.Provider(provider)
 		if err != nil {
@@ -57,6 +59,28 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		w.Header().Set("Location", loginUrl)
 		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("プロバイダーの取得に失敗しました:", provider, "-", err)
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("認証を完了できませんでした", provider, "-", err)
+		}
+
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("ユーザーの取得に失敗しました", provider, "-", err)
+		}
+
+		authCookieValue := objx.New(map[string]interface{}{"name": user.Name()}).MustBase64()
+		http.SetCookie(w, &http.Cookie{Name: "auth", Value: authCookieValue, Path: "/"})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "アクション%sには非対応です", action)
